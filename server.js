70ea0c48e9010aef4fb8a5038c131362d79a3e05
Andras Sevcsik <sevcsik@gmail.com>
2012-02-05 05:24:51 +0100

    custom port support

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 var http = require('http');
 var router = require('./router.js');
 
-var server = http.createServer(router.onRequest);
-server.listen(8080);
+// check for port number in arguments
+var port = 8080;
+if ((i = process.argv.indexOf('--port')) != -1 &&
+    i+1 in process.argv)
+{
+  port = parseInt(process.argv[i+1]);
+  if (!port) throw 'Invalid port: ' + process.argv[i+1];
+}
 
+var server = http.createServer(router.onRequest);
+console.log('listening on ' + port + ', have fun.');
+server.listen(port);
 
