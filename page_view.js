e8a6bf083dedb5697d55c6420647debab5ae913b
Andras Sevcsik <sevcsik@gmail.com>
2012-02-07 06:31:05 +0100

    working hopefully :)

--- page_view.js
+++ page_view.js
@@ -1,5 +1,69 @@
-exports.handle = function (request, response)
+fs = require('fs');
+
+var page_cache = {};
+var template = null;
+var no_cache = process.no_cache;
+
+function renderPage(page_name, template, response)
 {
-  response.write('page view');
-  response.end();
+  // load page
+  var output;
+  if (!no_cache && page_name in page_cache)
+  {
+    output = template.replace('{{ARTICLE}}', page_cache[page_name]);
+    response.write(output);
+    response.end();
+  }
+  else
+  {
+    fs.readFile('pages' + '/' + page_name + '.html', 'utf-8',
+                function(err, data)
+    {
+      if (err)
+      {
+        response.writeHead(404, 'Page Missing');
+        response.end();
+      }
+      else
+      {
+        console.log('page cached: ' + page_name);
+        var output = template.replace('{{ARTICLE}}', data);
+        response.write(output);
+        response.end();
+
+        // put in page_cache
+        if (!no_cache) page_cache[page_name] = data;
+      }
+    });
+  }
 }
+
+exports.renderTemplate = function(request, response)
+{
+  // strip slash from url
+  var page_name = request.url.replace('/', '');
+  if (!page_name) page_name = 'root';
+
+  // load template
+  if (!template)
+  {
+    fs.readFile('template.html', 'utf-8', function(err, data)
+    {
+      if (err)
+      {
+        response.writeHead(404, 'Template Missing');
+        response.end(); // TODO: generic error module
+      }
+      else
+      {
+        console.log('template cached');
+        template = data;
+        renderPage(page_name, template, response);
+      }
+    });
+  }
+  else renderPage(page_name, template, response);
+
+  // only keep template in memory, if cache is enabled
+  if (no_cache) template = null;
+};
