02270e5450740f6b9edc2709aeff02ac0be84d6a
Andras Sevcsik <sevcsik@gmail.com>
2012-02-20 08:44:49 +0400

    javascript navigation working

--- static/js/template.js
+++ static/js/template.js
@@ -13,19 +13,6 @@ var page_cache = {};
  */
 function onReady()
 {
-  $('#menu_slider > a').each(function(i, e)
-  {
-    // add onclick to each menu item
-    $(e).click(changePage);
-    var page = $(e).attr('data-page');
-
-    if (page != page_name) // don't load active page again
-      $.ajax('ajax/pages/' + page, { success: onPageLoad });
-
-    // add entries in page_cache
-    page_cache[page] = null;
-  });
-
   // bind browser navigation buttons to changePage
   window.onpopstate = changePage;
 
@@ -37,7 +24,36 @@ function onReady()
   page_cache[page_name] = initial_page;
 
   // get initial_page.content from content element
-  initial_page.content = $('#content').html();
+  initial_page.content = $('#content > .page').html();
+
+  $('#content').html('');
+
+  $('#menu_slider > a').each(function(i, e)
+  {
+    // add onclick to each menu item
+    $(e).click(changePage);
+    var page = $(e).attr('data-page');
+
+    if (page != page_name) // don't load active page again
+      $.ajax('ajax/pages/' + page, { success: onPageLoad });
+
+    // add entries in page_cache
+    if (!(page in page_cache)) page_cache[page] = null;
+
+    // insert page element to DOM
+    var page_div = $('<div></div>')
+      .addClass('page')
+      .attr('data-page', page);
+
+    // insert content, if it's already available (initial page)
+    if (page_cache[page]) page_div.html(page_cache[page].content)
+    else page_div.addClass('loading');
+
+    $('#content').append(page_div);
+  });
+
+  // focus on active page
+  $('#content').attr('class', 'focus-' + page_name);
 }
 
 /**
@@ -62,11 +78,16 @@ function changePage(event)
     history.pushState({ 'page': page }, page, page == 'root' ? '/' : page);
   }
 
-  // slide menu
+  // slide menu and content
   $('#menu').attr('class', 'focus-' + page);
+  $('#content').attr('class', 'focus-' + page);
 
   // set page_name global
   page_name = page;
+
+  // set page title
+  if (page_cache[page])
+    $('title').html('sevdev: ' + page_cache[page].title);
 
   return false;
 }
@@ -77,6 +98,9 @@ function changePage(event)
 function onPageLoad(data)
 {
   page_cache[data.name] = data;
+  $('#content [data-page="' + data.name + '"]')
+    .removeClass('loading')
+    .html(data.content);
 }
 
 $(document).ready(onReady);
