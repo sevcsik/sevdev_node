1b8017bc91b5ed7c5b7f0fc97170a0a3c545fdc0
Andras Sevcsik <sevcsik@gmail.com>
2013-01-29 19:38:37 -0500

    undeclared variable fix

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ var router = require('./router.js');
 var port = 8080;
 
 // use PORT environment variable if available
-if (process.env['PORT']) {
-  port = parseInt(process.env['PORT'], 10);
-} else if ((i = process.argv.indexOf('--port')) !== -1 &&
-     process.argv.length > i+1) {
+if (process.env.PORT) {
+  port = parseInt(process.env.PORT, 10);
+} else if (i !== -1 && process.argv.length > i+1) {
+  var i = process.argv.indexOf('--port');
   // check for port number in arguments
   port = parseInt(process.argv[i+1], 10);
   if (!port) { throw 'Invalid port: ' + process.argv[i+1]; }
