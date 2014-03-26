d48d4e84e96ea3cb72477b70cafaba6c42ef651c
Andras Sevcsik <sevcsik@gmail.com>
2014-03-26 16:51:01 +0100

    fixed port number handling, removed PORT variable

--- server.js
+++ server.js
@@ -3,14 +3,13 @@ var router = require('./router.js');
 
 var port = 8080;
 
-// use PORT environment variable if available
-if (process.env.PORT) {
-  port = parseInt(process.env.PORT, 10);
-} else if (i !== -1 && process.argv.length > i+1) {
-  var i = process.argv.indexOf('--port');
+if (process.argv.indexOf('--port') !== -1) {
   // check for port number in arguments
-  port = parseInt(process.argv[i+1], 10);
-  if (!port) { throw 'Invalid port: ' + process.argv[i+1]; }
+  var portString = process.argv[process.argv.indexOf('--port') + 1];
+
+  port = +portString;
+
+  if (!port) throw 'Invalid port: ' + portString;
 }
 
 var server = http.createServer(router.onRequest);
